b6407dea01d0794a11b87d3836f3f4badc373eb1
Thomas Baillie <2439407+ThomasShaped@users.noreply.github.com>
2018-11-01 23:10:29 +0100

    Add getPokemon

--- PokemonManager.js
+++ PokemonManager.js
@@ -28,9 +28,22 @@ class PokemonManager {
    */
   generateEncounter() {
     console.log('Generating encounter');
-    
 
+    const pokemon = this.getPokemon();
+    console.log(pokemon);
     return;
+  }
+
+  /**
+   * Gets the specified Pokemon, or a random one
+   * @param {string} [Id] The ID of the pokemon
+   */
+  getPokemon(Id) {
+    if (Id) {
+      return this.pokemon[Id];
+    } else {
+      return this.pokemon[Math.floor(Math.random() * Object.keys(this.pokemon).length)];
+    }
   }
 
   /**
